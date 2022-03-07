9f6b84bf8e03e4d013a6d6f078100e3622c94b53
Huffle Votom <ten.crypto@outlook.com>
2022-03-07 18:02:37 -0500

    feat: iconos reportes

--- src/containers/Sidebar/SidebarContent.js
+++ src/containers/Sidebar/SidebarContent.js
@@ -62,25 +62,25 @@ const SidebarContent = ({ sidebarCollapsed, setSidebarCollapsed }) => {
           >
             <Menu.Item key="reporte1">
               <Link to="/reporte1">
-                <i className="icon icon-widgets" />
+                <i className="icon icon-auth-screen" />
                 <span>Analisis de Ordenes y Pacientes vs Concluidos</span>
               </Link>
             </Menu.Item>
             <Menu.Item key="reporte2">
               <Link to="/reporte2">
-                <i className="icon icon-widgets" />
+                <i className="icon icon-shopping-cart" />
                 <span>Analisis de Venta por Mes</span>
               </Link>
             </Menu.Item>
             <Menu.Item key="reporte3">
               <Link to="/reporte3">
-                <i className="icon icon-widgets" />
+                <i className="icon icon-editor" />
                 <span>Examenes mas Rotados</span>
               </Link>
             </Menu.Item>
             <Menu.Item key="reporte4">
               <Link to="/reporte4">
-                <i className="icon icon-widgets" />
+                <i className="icon icon-hotel-booking" />
                 <span>Atencion de espcialidades por mes</span>
               </Link>
             </Menu.Item>
@@ -89,26 +89,26 @@ const SidebarContent = ({ sidebarCollapsed, setSidebarCollapsed }) => {
               popupClassName={getNavStyleSubMenuClass(navStyle)}
               title={
                 <span>
-                  <i className="icon icon-icon" />
+                  <i className="icon icon-data-display" />
                   <span>Power BI</span>
                 </span>
               }
             >
               <Menu.Item key="especialidadDetallado">
                 <Link to="/powerBi/especialidadDetallado">
-                  <i className="icon icon-icon" />
+                  <i className="icon icon-tree" />
                   <span>Tablero Especialidad - Detallado</span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="especialidadAcumulado">
                 <Link to="/powerBi/especialidadAcumulado">
-                  <i className="icon icon-icon" />
+                  <i className="icon icon-stats" />
                   <span>Tablero Especialidad - Acumulado</span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="generalVenta">
                 <Link to="/powerBi/generalVenta">
-                  <i className="icon icon-icon" />
+                  <i className="icon icon-timeline" />
                   <span>Tablero General - Venta</span>
                 </Link>
               </Menu.Item>
